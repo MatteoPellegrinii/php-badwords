3e4952b539ddb1bba1121ac69ebbc41134c87a24
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-06-24 15:22:11 +0200

    primo push

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+$paragrafo = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio porro, tempora sint eius ducimus ipsa inventore minima voluptatum doloribus pariatur veritatis omnis similique sunt sequi ullam sit? Harum, quibusdam magni.'
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Questo è il mio paragrafo:  <br> <?php echo $paragrafo ?>   </h3>
+    <h3>E' lungo: <?php echo strlen($paragrafo) ?> caratteri </h3>
+    <form action="" method="get" >
+        <label for="parola">Parola:</label>
+        <input type="text" name="parola" id="parola">
+        <button>Nascondi parola</button>
+    </form>
+    
+</body>
+</html>
